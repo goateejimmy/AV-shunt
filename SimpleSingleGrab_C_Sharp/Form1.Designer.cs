fba08703f3b1245073bd4647c10a277a4b95dc2b
goateejimmy <47892327+goateejimmy@users.noreply.github.com>
2021-10-20 19:49:26 +0800

    add file

---
+++ SimpleSingleGrab_C_Sharp/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Sandkasten_CSharp
+{
+    partial class SimpleSingleGrab_C_Sharp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SimpleSingleGrab_C_Sharp));
+            this.DisplayWindow = new System.Windows.Forms.PictureBox();
+            this.Button_Quit = new System.Windows.Forms.Button();
+            this.Refresh = new System.Windows.Forms.Button();
+            this.Live = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.connect = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DisplayWindow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DisplayWindow
+            // 
+            this.DisplayWindow.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DisplayWindow.Location = new System.Drawing.Point(12, 11);
+            this.DisplayWindow.Name = "DisplayWindow";
+            this.DisplayWindow.Size = new System.Drawing.Size(751, 510);
+            this.DisplayWindow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.DisplayWindow.TabIndex = 0;
+            this.DisplayWindow.TabStop = false;
+            this.DisplayWindow.Click += new System.EventHandler(this.DisplayWindow_Click);
+            // 
+            // Button_Quit
+            // 
+            this.Button_Quit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Button_Quit.Location = new System.Drawing.Point(806, 78);
+            this.Button_Quit.Name = "Button_Quit";
+            this.Button_Quit.Size = new System.Drawing.Size(100, 28);
+            this.Button_Quit.TabIndex = 1;
+            this.Button_Quit.Text = "Quit";
+            this.Button_Quit.UseVisualStyleBackColor = true;
+            this.Button_Quit.Click += new System.EventHandler(this.Quit_Click);
+            // 
+            // Refresh
+            // 
+            this.Refresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Refresh.Location = new System.Drawing.Point(806, 12);
+            this.Refresh.Name = "Refresh";
+            this.Refresh.Size = new System.Drawing.Size(100, 28);
+            this.Refresh.TabIndex = 2;
+            this.Refresh.Text = "Single Frame";
+            this.Refresh.UseVisualStyleBackColor = true;
+            this.Refresh.Click += new System.EventHandler(this.Refresh_Click);
+            // 
+            // Live
+            // 
+            this.Live.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Live.Location = new System.Drawing.Point(806, 44);
+            this.Live.Name = "Live";
+            this.Live.Size = new System.Drawing.Size(100, 28);
+            this.Live.TabIndex = 3;
+            this.Live.Text = "Live";
+            this.Live.UseVisualStyleBackColor = true;
+            this.Live.Click += new System.EventHandler(this.Live_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(806, 178);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 20);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // connect
+            // 
+            this.connect.Location = new System.Drawing.Point(807, 217);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(99, 25);
+            this.connect.TabIndex = 5;
+            this.connect.Text = "connect";
+            this.connect.UseVisualStyleBackColor = true;
+            this.connect.Click += new System.EventHandler(this.connect_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(807, 297);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(2);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(99, 22);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(769, 343);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(2);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(166, 45);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Value = 10;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(781, 486);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 22);
+            this.textBox1.TabIndex = 7;
+            // 
+            // SimpleSingleGrab_C_Sharp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(937, 532);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.connect);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Live);
+            this.Controls.Add(this.Refresh);
+            this.Controls.Add(this.Button_Quit);
+            this.Controls.Add(this.DisplayWindow);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(320, 225);
+            this.Name = "SimpleSingleGrab_C_Sharp";
+            this.Text = "SingleGrab";
+            this.Load += new System.EventHandler(this.SimpleSingleGrab_C_Sharp_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DisplayWindow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox DisplayWindow;
+        private System.Windows.Forms.Button Button_Quit;
+        private System.Windows.Forms.Button Refresh;
+        private System.Windows.Forms.Button Live;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button connect;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ SimpleSingleGrab_C_Sharp/Form1.cs
@@ -0,0 +1,196 @@
+﻿//===========================================================================//
+//                                                                           //
+//  Copyright (C) 2006 - 2017                                                //
+//  IDS Imaging GmbH                                                         //
+//  Dimbacherstr. 6-8                                                        //
+//  D-74182 Obersulm-Willsbach                                               //
+//                                                                           //
+//  The information in this document is subject to change without            //
+//  notice and should not be construed as a commitment by IDS Imaging GmbH.  //
+//  IDS Imaging GmbH does not assume any responsibility for any errors       //
+//  that may appear in this document.                                        //
+//                                                                           //
+//  This document, or source code, is provided solely as an example          //
+//  of how to utilize IDS software libraries in a sample application.        //
+//  IDS Imaging GmbH does not assume any responsibility for the use or       //
+//  reliability of any portion of this document or the described software.   //
+//                                                                           //
+//  General permission to copy or modify, but not for profit, is hereby      //
+//  granted,  provided that the above copyright notice is included and       //
+//  reference made to the fact that reproduction privileges were granted     //
+//  by IDS Imaging GmbH.                                                     //
+//                                                                           //
+//  IDS cannot assume any responsibility for the use or misuse of any        //
+//  portion of this software for other than its intended diagnostic purpose  //
+//  in calibrating and testing IDS manufactured cameras and software.        //
+//                                                                           //
+//===========================================================================//
+
+/// Developer Note: I tried to keep it as simple as possible.
+/// Therefore there are no functions asking for the newest driver software or freeing memory beforehand, etc.
+/// This program is solely meant to show one of the simplest ways to interact with an IDS Camera through the uEye API.
+/// (XS Cameras are not supported)
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Sandkasten_CSharp
+{
+    public partial class SimpleSingleGrab_C_Sharp : Form
+    {
+
+        public delegate void scroll(object sender, EventArgs e);
+
+        //serialport preparetion
+        SerialPort Port = new SerialPort();
+        string[] PortName;
+        Byte[] buffer = new Byte[5];
+
+
+        //To use the .NET interface you have to create a uEye .NET object
+        private uEye.Camera Camera = new uEye.Camera();
+        IntPtr DisplayHandle = IntPtr.Zero;
+
+        public SimpleSingleGrab_C_Sharp()
+        {
+            InitializeComponent();
+            DisplayHandle = DisplayWindow.Handle;
+
+            //Initialize the camera
+            Camera.Init();
+
+            //Allocate an default image memory
+            Camera.Memory.Allocate();
+
+            Camera.EventFrame += onFrameEvent;
+
+            //Capture a single image with
+            Camera.Acquisition.Freeze();
+        }
+        
+
+
+        private void onFrameEvent(object sender, EventArgs e)
+        {
+            uEye.Camera Camera = sender as uEye.Camera;
+
+            //Display the image on the screen
+            Camera.Display.Render( DisplayHandle, uEye.Defines.DisplayRenderMode.FitToWindow);
+        }
+
+
+
+        //The following are key definitions
+        private void Quit_Click(object sender, EventArgs e)
+        {
+            //Free the allocated image memory and closes the program
+            Camera.Exit();
+            Close();
+        }
+
+        private void Refresh_Click(object sender, EventArgs e)
+        {
+            //Capture a single frame
+            Camera.Acquisition.Freeze();
+        }
+
+        private void Live_Click(object sender, EventArgs e)
+        {
+            //Capture a live image
+            Camera.Acquisition.Capture();
+
+            //Currently we are working without Ring-Buffer
+            //For the purpose of showing this example that's enough
+            //But if you want to do some Image data processing you'll need to adjust the settings
+        }
+
+        private void DisplayWindow_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SimpleSingleGrab_C_Sharp_Load(object sender, EventArgs e)
+        {
+            PortName = SerialPort.GetPortNames();
+            comboBox1.Items.AddRange(PortName);
+          
+
+        }
+
+        private void connect_Click(object sender, EventArgs e)
+        {
+            if (PortName.Length > 0)
+            {
+                Port.PortName = comboBox1.Text;
+                Port.BaudRate = 57600;
+                Port.Parity = Parity.None;
+                Port.StopBits = StopBits.One;
+                //Port.Encoding = Encoding.GetEncoding("utf-32");
+
+                Port.Open();
+            }
+        }
+        public void SetCurrent(int value)
+        {
+
+
+            buffer[0] = 0x02;
+            buffer[1] = 0x37;
+
+            buffer[2] = 0x01;
+
+            buffer[3] = 0x01;
+
+            string hexvalue = Convert.ToString(value, 16);
+            //   buffer[5] = Convert.ToByte( "0x" + Convert.ToString(value, 16));
+            buffer[4] = Convert.ToByte(hexvalue, 16);
+
+
+
+            Port.Write(AddCRC(buffer), 0, AddCRC(buffer).Length);
+            
+        }
+
+        private byte[] AddCRC(byte[] command)//CRC
+        {
+            int CRC = 0;
+
+            byte[] commandWithCRC = new byte[command.Length + 1];
+            for (int i = 0; i < command.Length; i++)
+                CRC = CRC + command[i];
+
+            Array.Copy(command, 0, commandWithCRC, 0, command.Length);
+            commandWithCRC[commandWithCRC.Length - 1] = (byte)CRC;
+            //richTextBox1.Text = String.Format("{0:X}", CRC.ToString());
+
+            return commandWithCRC;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            int light_value = trackBar1.Value;
+
+            //  MessageBox.Show(XORstring("$12064"));
+            numericUpDown1.Value = light_value;
+
+            SetCurrent(light_value);
+            textBox1.Text = "sendind....";
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            int value =(int)numericUpDown1.Value;
+            trackBar1.Value = value;
+            //scroll Scroll = new scroll(trackBar1_Scroll);
+            trackBar1_Scroll(sender, e);
+            //Scroll(sender,e);
+        }
+    }
+}
