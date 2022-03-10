4d3a5421500c387842f62414a3d5902c10431b0d
goateejimmy <jimkuoforever20@gmail.com>
2022-03-10 18:22:17 +0800

    video shooting

---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(35, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(402, 311);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(535, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 68);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(535, 196);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 91);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(535, 309);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(119, 57);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(239, 388);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(197, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu;
+using Emgu.Util;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.Threading;
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        int i;
+        int fourcc;
+        int width;
+        int height;
+        int fps;
+        bool flag;
+        List<Mat> buffer = new List<Mat>();
+        VideoCapture capture;
+        VideoWriter writer;
+        Image<Bgr, byte> frame;
+        Image<Bgr, byte> copy;
+        Thread forcap;
+        public Form1()
+        {
+            i = 0;
+            InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            flag = false;
+            capture = new VideoCapture(0);
+            capture.SetCaptureProperty(Emgu.CV.CvEnum.CapProp.Fps, 30);
+            capture.SetCaptureProperty(Emgu.CV.CvEnum.CapProp.Buffersuze,3);
+            fourcc = Convert.ToInt32(capture.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.FourCC));
+            width = Convert.ToInt32(capture.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.FrameWidth));
+            height = Convert.ToInt32(capture.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.FrameHeight));
+            fps = Convert.ToInt32(capture.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.Fps));
+           
+
+           
+            frame = new Image<Bgr, byte>(capture.Width, capture.Height);
+           
+        }
+
+        //private void Capture_ImageGrabbed(object sender, EventArgs e)
+        //{
+        //    if (capture.Grab())
+        //    { capture.Retrieve(frame, 0); }
+           
+        //    pictureBox1.Image = frame.Bitmap;
+        //    writer.Write(frame.Mat);
+        //}
+        private void cap()
+        {
+            while (true)
+            {
+                if (capture.Grab())
+                {
+                    Console.WriteLine(i);
+
+
+                    //capture.Retrieve(frame, 0);
+
+                 
+                   
+
+                    //pictureBox1.Image = frame.Bitmap;
+
+                    //if (flag)
+                    //{
+                    //    buffer.Add(frame.Mat);
+                    //}
+                    i = i + 1;
+                }
+            }
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            writer = new VideoWriter(@"D:\jim\thesis\testvideo\testvid.mp4", fourcc, fps, new Size(width, height), true);
+            forcap = new Thread(cap);
+            flag = true;
+            forcap.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            flag = false;
+           
+            MessageBox.Show("done");
+            
+            forcap.Abort();
+            var buf_array = buffer.ToArray();
+            textBox1.Text =buf_array.Length.ToString();
+            foreach (Mat i in buf_array)
+            {
+                writer.Write(i);
+            }
+            
+            writer.Dispose();
+        }
+    }
+}
